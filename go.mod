ac16c31d89e5029b3b5e8c2229ec8402bd353d1b
naskovik <nazarskoreyko@gmail.com>
2025-02-15 20:57:09 +0100

    setting up consensus

--- go.mod
+++ go.mod
@@ -16,17 +16,25 @@ require (
 
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-metrics v0.5.4 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
 	github.com/hashicorp/go-msgpack/v2 v2.1.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-sockaddr v1.0.5 // indirect
 	github.com/hashicorp/golang-lru v1.0.2 // indirect
 	github.com/hashicorp/memberlist v0.5.2 // indirect
+	github.com/hashicorp/raft v1.7.2 // indirect
+	github.com/hashicorp/raft-boltdb v0.0.0-20250113192317-e8660f88bcc9 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/miekg/dns v1.1.56 // indirect
 	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
---
+++ internal/log/distributed.go
@@ -0,0 +1,279 @@
+package log
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/hashicorp/raft"
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+	api "github.com/naskovik/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+type DistributedLog struct {
+	config Config
+	log    *Log
+	raft   *raft.Raft
+}
+
+func (d *DistributedLog) setupLog(dataDir string) error {
+	logDir := filepath.Join(dataDir, "log")
+	var err error
+	if err = os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+
+	d.log, err = NewLog(logDir, d.config)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func (d *DistributedLog) setupRaft(dataDir string) error {
+	fsm := &fsm{log: d.log}
+
+	logDir := filepath.Join(dataDir, "raft", "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+	logConfig := d.config
+	logConfig.Segment.InitialOffset = 1
+	logStore, err := newLogStore(logDir, logConfig)
+	if err != nil {
+		return err
+	}
+
+	stableStore, err := raftboltdb.NewBoltStore(
+		filepath.Join(dataDir, "raft", "stable"),
+	)
+
+	if err != nil {
+		return nil
+	}
+
+	retain := 1
+	snapshotStore, err := raft.NewFileSnapshotStore(
+		filepath.Join(dataDir, "raft"),
+		retain,
+		os.Stderr,
+	)
+	if err != nil {
+		return err
+	}
+
+	maxPool := 5
+	timeout := 10 * time.Second
+	transport := raft.NewNetworkTransport(
+		d.config.Raft.StreamLayer,
+		maxPool,
+		timeout,
+		os.Stderr,
+	)
+
+	config := raft.DefaultConfig()
+	config.LocalID = d.config.Raft.LocalID
+	if d.config.Raft.HeartbeatTimeout != 0 {
+		config.HeartbeatTimeout = d.config.Raft.HeartbeatTimeout
+	}
+
+	if d.config.Raft.ElectionTimeout != 0 {
+		config.ElectionTimeout = d.config.Raft.ElectionTimeout
+	}
+
+	if d.config.Raft.LeaderLeaseTimeout != 0 {
+		config.LeaderLeaseTimeout = d.config.Raft.LeaderLeaseTimeout
+	}
+
+	if d.config.Raft.CommitTimeout != 0 {
+		config.CommitTimeout = d.config.Raft.CommitTimeout
+	}
+
+	d.raft, err = raft.NewRaft(
+		config,
+		fsm,
+		logStore,
+		stableStore,
+		snapshotStore,
+		transport,
+	)
+	if err != nil {
+		return err
+	}
+
+	hasState, err := raft.HasExistingState(
+		logStore,
+		stableStore,
+		snapshotStore,
+	)
+	if err != nil {
+		return err
+	}
+
+	if d.config.Raft.Bootstrap && !hasState {
+		config := raft.Configuration{
+			Servers: []raft.Server{{
+				ID:      config.LocalID,
+				Address: transport.LocalAddr(),
+			}},
+		}
+		err = d.raft.BootstrapCluster(config).Error()
+	}
+
+	return err
+}
+
+func (d *DistributedLog) Append(record *api.Record) (uint64, error) {
+	res, err := d.apply(
+		AppendResquestType,
+		&api.ProduceRequest{Record: record},
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	return res.(*api.ProduceResponse).Offset, nil
+
+}
+
+func (d *DistributedLog) apply(reqType RequestType, req proto.Message) (interface{}, error) {
+	var buf bytes.Buffer
+	_, err := buf.Write([]byte{byte(reqType)})
+	if err != nil {
+		return nil, err
+	}
+	b, err := proto.Marshal(req)
+	_, err = buf.Write(b)
+	if err != nil {
+		return nil, err
+	}
+
+	timeout := 10 * time.Second
+	future := d.raft.Apply(buf.Bytes(), timeout)
+	if future.Error() != nil {
+		return nil, future.Error()
+	}
+	res := future.Response()
+	if err, ok := res.(error); ok {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+var _ raft.FSM = (*fsm)(nil)
+
+type fsm struct {
+	log *Log
+}
+
+type RequestType uint8
+
+const AppendResquestType RequestType = 0
+
+func (f *fsm) Apply(record *raft.Log) interface{} {
+	buf := record.Data
+	reqtype := RequestType(buf[0])
+	switch reqtype {
+	case AppendResquestType:
+		return f.applyAppend(buf)
+	}
+	return nil
+}
+
+func (f *fsm) applyAppend(b []byte) interface{} {
+	var req api.ProduceRequest
+	err := proto.Unmarshal(b, &req)
+	if err != nil {
+		return err
+	}
+	offset, err := f.log.Append(req.Record)
+	if err != nil {
+		return err
+	}
+	return &api.ProduceResponse{Offset: offset}
+
+}
+
+func (f *fsm) Restore(r io.ReadCloser) error {
+	return nil
+}
+
+func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	r := f.log.Reader()
+	return &snapshot{reader: r}, nil
+}
+
+var _ raft.FSMSnapshot = (*snapshot)(nil)
+
+type snapshot struct {
+	reader io.Reader
+}
+
+func (s *snapshot) Persist(sink raft.SnapshotSink) error {
+	if _, err := io.Copy(sink, s.reader); err != nil {
+		_ = sink.Cancel()
+		return err
+	}
+	return sink.Close()
+}
+
+func (s *snapshot) Release() {}
+
+type logStore struct {
+	*Log
+}
+
+func newLogStore(dir string, c Config) (*logStore, error) {
+	log, err := NewLog(dir, c)
+	if err != nil {
+		return nil, err
+	}
+	return &logStore{log}, nil
+}
+
+func (l *logStore) FirstIndex() (uint64, error) {
+	return l.LowestOffset()
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+	return l.HighestOffset()
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
+		return err
+	}
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
+	return nil
+}
+
+func (l *logStore) StoreLog(record *raft.Log) error {
+	return l.StoreLogs([]*raft.Log{record})
+}
+
+func (l *logStore) StoreLogs(records []*raft.Log) error {
+	for _, record := range records {
+		if _, err := l.Append(&api.Record{
+			Value: record.Data,
+			Term:  record.Term,
+			Type:  uint32(record.Type),
+			// TODO
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+	return l.Truncate(max)
+}
